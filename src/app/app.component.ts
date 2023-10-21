2fac591541c3f988f4292d41513115a21a91810c
jeremycastillo <jeremy.castillo14@utexas.edu>
2023-10-21 14:14:19 -0500

    removing some bs. added bar/line chart example w/chartjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <router-outlet></router-outlet>
-  `,
+  template: `<app-bar-chart></app-bar-chart><br><br><br><app-line-chart></app-line-chart>`,
 })
 export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,34 +7,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule, Routes } from '@angular/router';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './layout/layout.component';
-import { HomeComponent } from './home/home.component';
-import { MenuItemComponent } from './layout/helper/menu-item/menu-item.component';
-import { MenuHeaderComponent } from './layout/helper/menu-header/menu-header.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'home',
-  },
-  {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: 'home',
-        component: HomeComponent,
-      },
-    ],
-  },
-];
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
 
 @NgModule({
   imports: [
@@ -46,19 +25,14 @@ const routes: Routes = [
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    RouterModule.forRoot(routes),
     MatProgressSpinnerModule,
     MatExpansionModule,
     MatMenuModule,
   ],
   declarations: [
     AppComponent,
-    LayoutComponent,
-    MenuItemComponent,
-    MenuHeaderComponent,
-    // PageHeaderComponent,
-    // HomeComponent,
-    // SalesComponent,
+    BarChartComponent,
+    LineChartComponent
   ],
   bootstrap: [AppComponent],
 })
