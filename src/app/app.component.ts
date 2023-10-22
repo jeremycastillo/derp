ea942b514c906ce451eb549447b979e10cd87443
jeremycastillo <jeremy.castillo14@utexas.edu>
2023-10-22 16:37:33 -0500

    adding more code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
+  <h2>Our Insurance Charts</h2>
+  <p style="color: purple;">Use the interactive charts below to explore the dataset</p>
   <app-bar-chart/>
   `,
 })
